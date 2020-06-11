a51b21143a730e6221a3749b2b3c15c01340cffb
tminei <34548743+tminei@users.noreply.github.com>
2020-06-11 17:36:08 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -19,38 +19,33 @@
 </head>
 <body>
 <form action="index.php" method="post">
-    <table id="main_table">
-        <table id="sub_table">
-            <tr>
-                <td>
 
-                    <input value="SCAN" type="submit" name="scan">
-                    <?php
-                    //ini_set('error_reporting', E_ALL);
-                    //ini_set('display_errors', 1);
-                    //ini_set('display_startup_errors', 1);
 
-                    if (isset($_POST['scan'])) {
+    <input value="SCAN" type="submit" name="scan">
+    <?php
+    //ini_set('error_reporting', E_ALL);
+    //ini_set('display_errors', 1);
+    //ini_set('display_startup_errors', 1);
 
-                        echo "<br>";
-                        echo "<br>";
-                        echo "Finded ONVIF device:";
-                        echo "<br>";
-                        $temp = shell_exec('python3 /home/shyneko/PycharmProjects/onvif/get_cams_as_json.py');
+    if (isset($_POST['scan'])) {
 
-                        $temp = json_decode($temp, true);
+        echo "<br><br><table width='25%' border='1' style='border-collapse: collapse'>";
+        $temp = shell_exec('python3 /home/shyneko/PycharmProjects/onvif/get_cams_as_json.py');
+
+        $temp = json_decode($temp, true);
 //                        var_dump($temp);
-                        foreach ($temp as $t) {
-                            echo($t["ip"] . ":" . $t["port"] . " -> " . $t["manufacturer"]);
-                            echo "<br>";
-                        }
-                    }
-                    ?>
+        echo "<tr><td  align='center' bgcolor='#f5deb3' style=' padding: 5px' colspan='3'>Found ONVIF device:</td></tr><tr><td  bgcolor='#dcdcdc' align='center' style='padding: 5px'>IP</td><td bgcolor='#dcdcdc' align='center' style='padding: 5px'>PORT</td><td  bgcolor='#dcdcdc' align='center' style='padding: 5px'>MANUFACTURER</td></tr>";
+        foreach ($temp as $t) {
+            echo "<tr><td align='center' style='padding: 5px'>";
+            echo("<a href=\"http://" . $t["ip"] . "\">" . $t["ip"] . "</a></td ><td align='center' style='padding: 5px'>" . "" . $t["port"] . "</td><td align='center' style='padding: 5px'>" . $t["manufacturer"] . "</td>");
+//            echo "<br>";
+            echo "</td></tr>";
+        }
+        echo "</table>";
+    }
+    ?>
 
-                </td>
-            </tr>
-        </table>
-    </table>
+
 </form>
 
 </body>
