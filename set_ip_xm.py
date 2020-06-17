b76c6bca61ef923e9da3bd7ecb2363b2affc04c3
tminei <34548743+tminei@users.noreply.github.com>
2020-06-17 10:14:29 +0300

    Add files via upload

--- set_ip_xm.py
+++ set_ip_xm.py
@@ -17,19 +17,11 @@ try:
         print(cam.get_info('NetWork.NetCommon.HostIP'))
         cam.set_info('NetWork.NetCommon.HostIP', new_ip_hex)
         cam.close()
+
         exit(0)
     else:
         exit(2)
-
-
-    # do_this = "http://{0}/cgi-bin/configManager.cgi?action=setConfig&Network.eth0.IPAddress={1}".format(old_ip, new_ip)
-    # # print(do_this)
-    # response = session.get(do_this)
-    # session.close()
-    # exit(response.status_code)
 except:
     exit(1)
-
-# http://10.10.0.85:8080/cgi-bin/configManager.cgi?action=setConfig&Network.eth0.IPAddress=192.168.1.11
 
 
