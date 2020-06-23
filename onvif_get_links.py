b04bce793fe7c8f197c84f93a316c6d9bf75db35
tminei <34548743+tminei@users.noreply.github.com>
2020-06-23 16:44:06 +0300

    Add files via upload

--- onvif_get_links.py
+++ onvif_get_links.py
@@ -1,5 +1,6 @@
 from onvif import ONVIFCamera
 import zeep
+import re
 
 
 def zeep_pythonvalue(self, xmlvalue):
@@ -14,6 +15,23 @@ media_service.GetProfiles()
 media_profile = media_service.GetProfiles()[0]
 token = media_profile["token"]
 # media_service.GetStreamUri({'ProfileToken':token})
+camType = "NaN"
+link = str(
+    media_service.GetStreamUri({'StreamSetup': {'Stream': 'RTP-Unicast', 'Transport': 'UDP'}, 'ProfileToken': token})[
+        "Uri"])
+lowLink = "NaN"
+pattern = 'rtsp:\/\/192\.168\.1\.[0-9]{1,3}:[0-9]{1,3}\/live\/main'
+result = re.match(pattern, link)
 
-print(media_service.GetStreamUri({'StreamSetup':{'Stream':'RTP-Unicast','Transport':'UDP'},'ProfileToken':token}))
-# print(media.GetStreamUri())
+if result:
+    camType = "IPC"
+else:
+    pattern = 'rtsp:\/\/192\.168\.1\.[0-9]{1,3}:[0-9]{1,3}\/cam'
+    result = re.match(pattern, link)
+    if result:
+        camType = "DAHUA"
+if camType == "IPC":
+    lowLink = link[:-4] + "sub"
+elif camType == "DAHUA":
+    lowLink = link[:link.find("subtype=") + 8] + "1" + link[link.find("subtype=") + 9:]
+print(link + "\n" + lowLink)
