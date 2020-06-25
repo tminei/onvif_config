3e51e7b7a29ebb72a0939aac8aefeecea67a5d9d
tminei <34548743+tminei@users.noreply.github.com>
2020-06-25 12:10:28 +0300

    Add files via upload

--- set_ip_xm.py
+++ set_ip_xm.py
@@ -4,6 +4,8 @@ from dvrip import DVRIPCam
 try:
     old_ip = str(sys.argv[1])
     new_ip_dec = str(sys.argv[2])
+    if new_ip_dec == old_ip:
+        exit(0)
     if new_ip_dec == "192.168.1.10":
         new_ip_hex = "0x0A01A8C0"
     elif new_ip_dec == "192.168.1.11":
@@ -12,16 +14,25 @@ try:
         new_ip_hex = "0x0C01A8C0"
     elif new_ip_dec == "192.168.1.13":
         new_ip_hex = "0x0D01A8C0"
-    cam = DVRIPCam(old_ip, "admin", "")
+    elif new_ip_dec == "192.168.1.14":
+        new_ip_hex = "0x0E01A8C0"
+    elif new_ip_dec == "192.168.1.15":
+        new_ip_hex = "0x0F01A8C0"
+    cam = DVRIPCam(old_ip, "admin")
     if cam.login():
-        print(cam.get_info('NetWork.NetCommon.HostIP'))
+        # cam.set_info('NetWork.NetDHCP[0].Enable', "False")
+        # cam.set_info('IPAdaptive.IPAdaptive', "False")
+        # print(cam.get_info('IPAdaptive.IPAdaptive'))
+        # print(cam.get_info('NetWork'))
+        # print(cam.get_info('NetWork.NetDHCP[0].Enable'))
         cam.set_info('NetWork.NetCommon.HostIP', new_ip_hex)
         cam.close()
+    # cam = DVRIPCam(new_ip_dec, "admin", "")
+    # if cam.login():
+    #     print(cam.get_info('NetWork.NetDHCP[0].Enable'))
+    #     cam.close()
 
-        exit(0)
-    else:
-        exit(2)
+
+
 except:
     exit(1)
-
-
